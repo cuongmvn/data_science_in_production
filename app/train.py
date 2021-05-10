436c231b81b08ece7725ab785d05ee1c40641e68
bachtn <alaa.bakhti@epita.fr>
2021-05-10 03:27:14 +0100

    extracted the pipeline functions to python modules

---
+++ app/train.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.metrics import mean_squared_log_error
+
+
+def compute_rmsle(y_test: np.ndarray, y_pred: np.ndarray, precision: int = 2) -> float:
+    rmsle = np.sqrt(mean_squared_log_error(y_test, y_pred))
+    return round(rmsle, precision)
